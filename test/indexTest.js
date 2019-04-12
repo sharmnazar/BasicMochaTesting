30b563628b68cbe7b076e3b0e3f1027b50dc87f1
sharmnazar <sharmnazar@gmail.com>
2019-04-12 11:14:22 -0400

    changed to strict equality

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,13 +4,13 @@ const index = require('../index');
 //Results
 sayHelloResult = index.sayHello();
 addNumberResult1 = index.addNum(2,5);
-addNumberResult2 = index.addNum(-2,-5);
+addNumberResult2 = index.addNum(2,-5);
 findSumResult = index.findSum([1,2,3])
 
 describe('index', function(){
     describe('sayHello()', function(){
         it('sayHello should return Hello', function(){
-            assert.equal(sayHelloResult, 'hello')
+            assert.strictEqual(sayHelloResult, 'hello')
         })
         it('sayHello should return type string', function(){
             assert.typeOf(sayHelloResult, 'string')
@@ -18,15 +18,15 @@ describe('index', function(){
     })
     describe('addNum() positives', function(){
         it('addNum should return 7', function(){
-            assert.equal(addNumberResult1, 7)
+            assert.strictEqual(addNumberResult1, 7)
         })
         it('addNum should return type number', function(){
             assert.typeOf(addNumberResult1, 'number')
         })
     })
     describe('addNum() negatives', function(){
-        it('addNum should return -7', function(){
-            assert.equal(addNumberResult2, -7)
+        it('addNum should return -3', function(){
+            assert.strictEqual(addNumberResult2, -3)
         })
         it('sayHello should return type number', function(){
             assert.typeOf(addNumberResult2, 'number')
@@ -37,7 +37,7 @@ describe('index', function(){
             assert.isAbove(findSumResult, 3)
         })
         it('findSum should return 6', function(){
-            assert.equal(findSumResult, 6)
+            assert.strictEqual(findSumResult, 6)
         })
         it('findSum should return type number', function(){
             assert.typeOf(findSumResult, 'number')
